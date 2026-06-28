327dde40adc533d40b233a75ceae1067e572626a
boseai25-cyber <bose.ai25@stellamaryscoe.edu.in>
2026-06-28 14:48:47 +0530

    Create Exe_2_g.js

---
+++ Lesson_2/Exe_2_g.js
@@ -0,0 +1,7 @@
+/*
+Setup in the Amazon project, go to the home page and add a toaster ($18.99) to your cart so you have 1 basketball, 1 t-shirt, and 1 toaster.Choose $4.99 shipping for the toaster
+
+Calculate the Total before tax.
+*/
+
+(2095 + 799 + 1899 + 499) / 100
