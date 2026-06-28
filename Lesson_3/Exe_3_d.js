67b3ac0983fbe5d626a6effbab2d55c0ea121899
boseai25-cyber <bose.ai25@stellamaryscoe.edu.in>
2026-06-28 22:59:04 +0530

    Create Exe_3_d.js

---
+++ Lesson_3/Exe_3_d.js
@@ -0,0 +1,3 @@
+/At a restaurant, you order 1 coffee ($5) and 1 bagel ($3). Using math, calculate the total cost, and using concatenation, create the text: 'Total cost: $_' (replace with the total you calculated).
+
+'Total cost:' + ' $' + (5 + 3)
