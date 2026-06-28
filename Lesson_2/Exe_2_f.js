e9edb58a377b2b8de88785b03fe46a9861d77433
boseai25-cyber <bose.ai25@stellamaryscoe.edu.in>
2026-06-28 14:44:42 +0530

    Create Exe_2_f.js

---
+++ Lesson_2/Exe_2_f.js
@@ -0,0 +1,7 @@
+/*
+Setup in the Amazon project, go to the home page and add a toaster ($18.99) to your cart so you have 1 basketball, 1 t-shirt, and 1 toaster.Choose $4.99 shipping for the toaster
+
+Calculate the cost of the products (before shipping and taxes). Hint: calculate in cents to avoid inaccuracies.
+*/
+
+(2095 + 799 + 1899) / 100
