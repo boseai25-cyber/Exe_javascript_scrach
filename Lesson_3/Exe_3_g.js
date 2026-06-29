cc37407e8519d8429ef4e6b01091da39a76404e5
boseai25-cyber <bose.ai25@stellamaryscoe.edu.in>
2026-06-29 10:47:17 +0530

    Create Exe_3_g.js

---
+++ Lesson_3/Exe_3_g.js
@@ -0,0 +1,3 @@
+/You order 1 coffee ($5.99) and 1 bagel ($2.95). Using math, calculate the total cost, and using concatenation, create the text: 'Total cost: $_' (hint: calculate in cents to avoid inaccuracies)
+
+'Total cost: $' + (599 + 295) / 100
