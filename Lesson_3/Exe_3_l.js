cfbe821d8b633a8b85e6bb0b06c449abaa8d7b83
boseai25-cyber <bose.ai25@stellamaryscoe.edu.in>
2026-06-29 11:11:08 +0530

    Create Exe_3_l.js

---
+++ Lesson_3/Exe_3_l.js
@@ -0,0 +1,7 @@
+/*
+Setup: in the Amazon project, update the cart to 2 basketballs ($20.95 each) with $4.99 shipping, and 2 t-shirts ($7.99 each) with $4.99 shipping.
+
+Create second line of text: 'Shipping & handling: $9.98' (use math).
+*/
+
+`shipping & handling: $${(499 + 499) / 100}`
