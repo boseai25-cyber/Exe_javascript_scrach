f6cf5cf1d131ebf76ce2962b0fb39c6ce88bf6a5
boseai25-cyber <bose.ai25@stellamaryscoe.edu.in>
2026-06-29 11:15:39 +0530

    Create Exe_3_m.js

---
+++ Lesson_3/Exe_3_m.js
@@ -0,0 +1,7 @@
+/*
+Setup: in the Amazon project, update the cart to 2 basketballs ($20.95 each) with $4.99 shipping, and 2 t-shirts ($7.99 each) with $4.99 shipping.
+
+Create third line: 'Total before tax: $67.86' (use math).
+*/
+
+`Total before tax: $${((2 * 2095 + 2 * 799) + (499 + 499)) / 100}`
